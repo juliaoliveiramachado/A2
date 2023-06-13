9b950b05edc9d04e67123be939a7adae478560b9
juliaoliveiramachado <133999730+juliaoliveiramachado@users.noreply.github.com>
2023-06-13 08:51:10 -0300

    Update oficial.py

--- oficial.py
+++ oficial.py
@@ -44,75 +44,45 @@ if resposta:
     print(diretorio["LINHA HIDRA DETOX CASPA"]['link'])
     st.write(diretorio["LINHA HIDRA DETOX CASPA"]['link'])
     st.image(diretorio["LINHA HIDRA DETOX CASPA"]['imagem'])
-
 elif resposta == "NÃO":
     resposta_adicional1 = st.checkbox("Você quer usar antes da escova?", ("SIM", "NÃO"))
+    print(resposta_adicional1)
     if resposta_adicional1 == "SIM": 
-       st.write(diretorio["LINHA QUERALISO"]['link'])
-       st.image(diretorio["LINHA QUERALISO"]['imagem'])
+        st.write(diretorio["LINHA QUERALISO"]['link'])
+        st.image(diretorio["LINHA QUERALISO"]['imagem'])
     elif resposta_adicional1 == "NÃO":
-         resposta_adicional2 = st.checkbox("Seu cabelo tem química?", ("SIM", "NÃO"))
-         if resposta_adicional2 == "SIM": 
+        resposta_adicional2 = st.checkbox("Seu cabelo tem química?", ("SIM", "NÃO"))
+        print(resposta_adicional2)
+        if resposta_adicional2 == "SIM":
             resposta_adicional3 = st.checkbox("Ele é pintado?", ("SIM", "NÃO"))
+            print(resposta_adicional3)
             if resposta_adicional3 == "SIM":
-               st.write(diretorio["LINHA COLORVIVE"]['link'])
-               st.image(diretorio["LINHA COLORVIVE"]['imagem'])
+                st.write(diretorio["LINHA COLORVIVE"]['link'])
+                st.image(diretorio["LINHA COLORVIVE"]['imagem'])
             elif resposta_adicional3 == "NÃO":
-               st.write(diretorio['LINHA REPARAÇÃO TOTAL PÓS QUÍMICA']['link'])
-               st.image(diretorio["LINHA REPARAÇÃO TOTAL PÓS QUÍMICA"]['imagem'])
-         elif resposta_adicional2 == "NÃO":
-               resposta_adicional4 = st.checkbox("Seu cabelo tem frizz?", ("SIM", "NÃO"))
-               if resposta_adicional4 == "SIM":
-                  st.write(diretorio['LINHA SUPREME CONTROL']['link'])
-                  st.image(diretorio['LINHA SUPREME CONTROL']['imagem'])
-               elif resposta_adicional4 == "NÃO":
-                    resposta_adicional5 = st.checkbox("Ele tem queda?", ("SIM", "NÃO"))
-                    if resposta_adicional5 == "SIM":
-                       st.write(diretorio['LINHA ARGININA']['link'])
-                       st.image(diretorio['LINHA ARGININA']['imagem'])
-                    elif resposta_adicional5 == "NÃO":
-                           resposta_adicional6 = st.checkbox("Ele está danificado?", ("SIM", "NÃO"))
-                           if resposta_adicional6 == 'SIM':
-                              resposta_adicional7 = st.checkbox("Ele está muito ou pouco danificado?", ("MUITO", "POUCO"))
-                              if resposta_adicional7 == 'MUITO':
-                                 st.write(diretorio['LINHA REPARAÇÃO TOTAL 5 PROFUNDA']['link'])
-                                 st.image(diretorio['LINHA REPARAÇÃO TOTAL 5 PROFUNDA']['imagem'])
-                              elif resposta_adicional7 == 'POUCO':
-                                 st.write(diretorio['LINHA REPARAÇÃO TOTAL 5']['link'])
-                                 st.image(diretorio['LINHA REPARAÇÃO TOTAL 5']['imagem'])
-                           elif resposta_adicional6 == 'NÃO':
-                                resposta_adicional8 = st.checkbox("Ele está desidratado?", ("SIM", "NÃO"))
-                                if resposta_adicional8 == 'SIM':
-                                   resposta_adicional9 = st.checkbox('Muito?' ("SIM", "NÃO"))
-                                   if resposta_adicional9 == 'SIM':
-                                      st.write(diretorio['LINHA HIALURONICO']['link'])
-                                      st.image(diretorio['LINHA HIALURONICO']['imagem'])
-                                   elif resposta_adicional9 == 'NÃO':
-                                      resposta_adicional10 = st.checkbox("Qual seu tipo de cabelo?", ("OLEOSO", "MISTO", "SECO"))
-                                      if resposta_adicional10 == 'OLEOSO':
-                                         st.write(diretorio['LINHA PURE HIALURONICO']['link'])
-                                         st.image(diretorio['LINHA PURE HIALURONICO']['imagem'])
-                                      elif resposta_adicional10 == 'MISTO':
-                                         st.write(diretorio['LINHA DETOX']['link'])
-                                         st.image(diretorio['LINHA DETOX']['imagem'])
-                                      elif resposta_adicional10 == 'SECO':
-                                         resposta_adicional11 = st.checkbox("Textura do cabelo?", ("LISO", "CACHEADO"))
-                                         if resposta_adicional11 == "SECO":
-                                             st.write(diretorio['LINHA OLEO EXTRAORDINARIO']['link'])
-                                             st.image(diretorio['LINHA OLEO EXTRAORDINARIO']['imagem'])
-                                         elif resposta_adicional11 == "CACHEADO":
-                                            st.write(diretorio['LINHA OLEO EXTRAORDINARIO CACHOS']['link'])
-                                            st.image(diretorio['LINHA OLEO EXTRAORDINARIO CACHOS']['imagem'])
-                                elif resposta_adicional8 == "NÃO":
-                                     resposta_adicional11 = st.checkbox("Qual é seu tipo de cabelo?", ("LISO", "CACHEADO"))
-                                     if resposta_adicional11 == "LISO":
-                                          resposta_adicional12 = st.checkbox("Quer mais liso ou mais longo?", ("LISO", "LONGO"))     
-                                          if resposta_adicional12 == "LISO":
-                                             st.write(diretorio['LINHA LISO DOS SONHOS']['link'])
-                                             st.image(diretorio['LINHA LISO DOS SONHOS']['imagem'])
-                                          elif resposta_adicional12 == 'LONGO':
-                                               st.write(diretorio['LINHA LONGO DOS SONHOS']['link'])
-                                               st.image(diretorio['LINHA LONGO DOS SONHOS']['imagem'])
-                                     elif resposta_adicional11 == "CACHEADO":
-                                          st.write(diretorio['LINHA CACHOS DOS SONHOS']['link'])
-                                          st.image(diretorio['LINHA CACHOS DOS SONHOS']['imagem'])
+                st.write(diretorio['LINHA REPARAÇÃO TOTAL PÓS QUÍMICA']['link'])
+                st.image(diretorio["LINHA REPARAÇÃO TOTAL PÓS QUÍMICA"]['imagem'])
+        elif resposta_adicional2 == "NÃO":
+            resposta_adicional4 = st.checkbox("Seu cabelo tem frizz?", ("SIM", "NÃO"))
+            print(resposta_adicional4)
+            if resposta_adicional4 == "SIM":
+                st.write(diretorio['LINHA SUPREME CONTROL']['link'])
+                st.image(diretorio['LINHA SUPREME CONTROL']['imagem'])
+            elif resposta_adicional4 == "NÃO":
+                resposta_adicional5 = st.checkbox("Ele tem queda?", ("SIM", "NÃO"))
+                print(resposta_adicional5)
+                if resposta_adicional5 == "SIM":
+                    st.write(diretorio['LINHA ARGININA']['link'])
+                    st.image(diretorio['LINHA ARGININA']['imagem'])
+                elif resposta_adicional5 == "NÃO":
+                    resposta_adicional6 = st.checkbox("Ele está danificado?", ("SIM", "NÃO"))
+                    print(resposta_adicional6)
+                    if resposta_adicional6 == 'SIM':
+                        resposta_adicional7 = st.checkbox("Ele está muito ou pouco danificado?", ("MUITO", "POUCO"))
+                        print(resposta_adicional7)
+                        if resposta_adicional7 == 'MUITO':
+                            st.write(diretorio['LINHA REPARAÇÃO TOTAL 5 PROFUNDA']['link'])
+                            st.image(diretorio['LINHA REPARAÇÃO TOTAL 5 PROFUNDA']['imagem'])
+                        elif resposta_adicional7 == 'POUCO':
+                            st.write(diretorio['LINHA REPARAÇÃO TOTAL 5']['link'])
+                            st.image(diretorio['LINHA REPARAÇÃO TOTAL 5']['imagem'])
