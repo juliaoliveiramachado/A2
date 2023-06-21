69d412a83ac9ca94ebe720630d49a8dd2165eea0
juliaoliveiramachado <133999730+juliaoliveiramachado@users.noreply.github.com>
2023-06-21 11:50:19 -0300

    Update lorealoficial.py

--- lorealoficial.py
+++ lorealoficial.py
@@ -101,3 +101,19 @@ elif caspaNao:
                  if quedaSim:
                      st.write(diretorio['LINHA ARGININA']['link'])
                      st.image(diretorio['LINHA ARGININA']['imagem'])
+
+                 elif quedaNao:
+                     st.write("6. Ele está danificado?")
+                     danificadoSim = st.checkbox("SIM", key,"danificado_1")
+                     danificadoNao = st.checkbox("NÃO", key,"danificado_2")
+
+                     if danificadoSim: 
+                         st.write("6.1 Ele está muito danificado?")
+                         superdanificadoSim = st.checkbox("SIM", key, "superdanificado_1")
+                         superdanificadoNao = st.checkbox("NÃO", key, "superdanificado_2")
+
+                        if superdanificadoSim:
+                           st.write(diretorio['LINHA REPARAÇÃO TOTAL 5 PROFUNDA']['link'])
+                           st.image(diretorio['LINHA REPARAÇÃO TOTAL 5 PROFUNDA']['imagem'])
+
+
