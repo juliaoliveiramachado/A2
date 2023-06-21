a43a9ef9ad2a4a436e4161bb178aba141210b2eb
juliaoliveiramachado <133999730+juliaoliveiramachado@users.noreply.github.com>
2023-06-21 12:21:03 -0300

    Update lorealoficial.py

--- lorealoficial.py
+++ lorealoficial.py
@@ -125,8 +125,8 @@ elif caspaNao:
                       desidratadoSim = st.checkbox("SIM", key="desidratado_1")
                       desidratadoNao = st.checkbox("NÃO", key="desidratado_2")
 
-                     if desidratadoSim: 
-                       st.write("7.1 Ele está muito desidratado?")
-                       muitodesidratadoSim = st.checkbox("SIM", key="muitodesidratado_1")
-                       muitodesidratadoNao = st.checkbox("NÃO", key="muitodesidratado_2")
+                   if desidratadoSim: 
+                      st.write("7.1 Ele está muito desidratado?")
+                      muitodesidratadoSim = st.checkbox("SIM", key="muitodesidratado_1")
+                      muitodesidratadoNao = st.checkbox("NÃO", key="muitodesidratado_2")
                     
