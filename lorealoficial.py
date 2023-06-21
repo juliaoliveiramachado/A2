276ce43b1e42796066d5f1595c0cb0cdf04c9299
juliaoliveiramachado <133999730+juliaoliveiramachado@users.noreply.github.com>
2023-06-21 12:18:41 -0300

    Update lorealoficial.py

--- lorealoficial.py
+++ lorealoficial.py
@@ -119,3 +119,14 @@ elif caspaNao:
                     elif superdanificadoNao: 
                         st.write(diretorio['LINHA REPARAÇÃO TOTAL 5']['link'])
                         st.image(diretorio['LINHA REPARAÇÃO TOTAL 5']['imagem'])
+
+                elif danificadoNao:
+                      st.write("7. Ele está desidratado?")
+                      desidratadoSim = st.checkbox("SIM", key="desidratado_1")
+                      desidratadoNao = st.checkbox("NÃO", key='desidratado_2")
+
+                     if desidratadoSim: 
+                       st.write("7.1 Ele está muito desidratado?")
+                       muitodesidratadoSim = st.checkbox("SIM", key="muitodesidratado_1")
+                       muitodesidratadoNao = st.checkbox("NÃO", key="muitodesidratado_2")
+                    
